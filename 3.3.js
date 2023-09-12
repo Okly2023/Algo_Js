97fcc27dff2ab8e42bfe446f480cef5db5ebd2e1
iradukunda okly <iradukundaokly@gmail.com>
2023-09-12 02:33:41 +0200

    add

---
+++ 3.3.js
@@ -0,0 +1,12 @@
+const fruits=['potato','celery','parsnip'];
+const newfruits=[];
+newfruits.push(...fruits);
+
+console.log(newfruits);
+
+
+
+const fruits1=['potato','celery','parsnip'];
+const newfruits1=[...fruits1];
+console.log(newfruits1);
+
